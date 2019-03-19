73ce6fa5f3a77b80eb886e62e282dbc8edbe3056
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2019-03-19 17:32:42 +0100

    Add class TunerPara to datatypes file. May has to be moved later on!

--- aixcal/datatypes.py
+++ aixcal/datatypes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-class TimeSeriesData():
+class TimeSeriesData(object):
     def __init__(self, filepath):
         self.data_type = None
         self.df = pd.DataFrame()
@@ -35,3 +35,30 @@ class SimTargetData(TimeSeriesData):
     def __init__(self, filepath):
         super().__init__(filepath)
         self.data_type = "SimTargetData"
+
+class TunerPara(object):
+    def __init__(self, name, initial_value, bounds = None):
+        self._assert_correct_input(name, initial_value, bounds)
+        self.name = name
+        self.initial_value = initial_value
+        self.bounds = bounds
+
+    def _assert_correct_input(self, name, initial_value, bounds):
+        """
+        Function to check whether the class paramters are correct or not.
+        :param name: Name of the tuner parameter
+        :param initial_value: Initial value
+        :param bounds: Tuple for lower and upper bound to the value
+        :return True if check is correct.
+        """
+        if not isinstance(name,str): raise TypeError("name has to be of type string")
+        if not isinstance(initial_value, (float,int)): raise TypeError("initial_value as to be of type float")
+        if bounds:
+            if not len(bounds)==2:
+                raise ValueError("The bounds object has to be of length 2 but has length%s"%len(bounds))
+            if not isinstance(bounds[0], (float, int)) or not isinstance(bounds[1], (float, int)):
+                raise TypeError("Given bounds are not of type float or int!")
+            if bounds[0] >= bounds[1]: raise ValueError("Lower bound is higher than upper bound!")
+            if initial_value < bounds[0] or initial_value > bounds[1]:
+                raise ValueError("The initial value lays outside of the given boundaries!")
+        return True
