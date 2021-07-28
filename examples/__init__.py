f16ba88d8eb15d7d67cb33214dea89691474d234
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2021-07-28 11:43:54 +0200

    Fix path

--- examples/__init__.py
+++ examples/__init__.py
@@ -13,7 +13,7 @@ def setup_fmu():
     if "win" in sys.platform:
         model_name = example_dir.joinpath("model", "PumpAndValve_windows.fmu")
     else:
-        model_name = data_dir.joinpath("model", "PumpAndValve_linux.fmu")
+        model_name = example_dir.joinpath("model", "PumpAndValve_linux.fmu")
 
     return FMU_API(cd=example_dir.joinpath("testzone"),
                    model_name=model_name)
