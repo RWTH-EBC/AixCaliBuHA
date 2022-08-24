3e90a0d12153b3e0367b4f395d6927cc83efff87
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2022-08-24 09:54:15 +0200

    chore: fix defaults

--- aixcalibuha/calibration/calibrator.py
+++ aixcalibuha/calibration/calibrator.py
@@ -115,9 +115,9 @@ class Calibrator(Optimizer):
         self.at_calibration = True  # Boolean to indicate if validating or calibrating
         # Extract kwargs for the visualizer
         visualizer_kwargs = {
-            "save_tsd_plot": kwargs.pop("save_tsd_plot", True),
-            "create_tsd_plot": kwargs.pop("create_tsd_plot", False),
-            "show_plot": kwargs.pop("show_plot", False),
+            "save_tsd_plot": kwargs.pop("save_tsd_plot", False),
+            "create_tsd_plot": kwargs.pop("create_tsd_plot", True),
+            "show_plot": kwargs.pop("show_plot", True),
             "show_plot_pause_time": kwargs.pop("show_plot_pause_time", 1e-3),
             "file_type": kwargs.pop("plot_file_type", "png"),
         }
