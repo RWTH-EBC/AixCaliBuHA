c02e1cbe9555cca6a5d0a5436e6ce3038d7aca25
Hendrik van der Stok <hendrik.stok@rwth-aachen.de>
2024-01-18 14:47:38 +0100

    update example for jupyter notebook

--- examples/e3_verbose_sensitivity_analysis_example.py
+++ examples/e3_verbose_sensitivity_analysis_example.py
@@ -1,14 +1,15 @@
-"""
-Example 3 verbose sensitivity analysis
-for the analysis of your model and the calibration process
+# ##Example 3 verbose sensitivity analysis
+# for the analysis of your model and the calibration process
 
-Goals of this part of the examples:
-1. Learn how to execute a verbose sensitivity analysis
-2. Learn the meaning of the results the analysis of your model
-3. Learn other sensitivity methods
-5. Learn time dependent sensitivity analysis
-4. Learn how to save the results for reproduction
-"""
+# Goals of this part of the examples:
+# 1. Learn how to execute a verbose sensitivity analysis
+# 2. Learn the meaning of the results and the analysis of your model
+# 3. Learn other sensitivity methods
+# 5. Learn time dependent sensitivity analysis
+# 4. Learn how to save the results for reproduction
+
+# Start by importing all relevant packages
+import pathlib
 import matplotlib.pyplot as plt
 from aixcalibuha import SobolAnalyzer, FASTAnalyzer, MorrisAnalyzer
 from aixcalibuha.data_types import merge_calibration_classes
@@ -31,7 +32,7 @@ def run_sensitivity_analysis(
     A comparison between different methods is shown.
     At the end the option to save a reproduction archive is shown.
 
-    :param [pathlib.Path, str] examples_dir:
+    :param [pathlib.Path] examples_dir:
         Path to the examples folder of AixCaliBuHA
     :param str example:
         Which example to run, "A" or "B"
@@ -47,17 +48,21 @@ def run_sensitivity_analysis(
     # some further settings for the analysis.
     # Let's first load the necessary simulation api:
     sim_api = setup_fmu(examples_dir=examples_dir, example=example, n_cpu=n_cpu)
-    # For performing a sensitivity analysis, we must define calibration classes which
-    # contain the objective on which the sensitivity will be calculated.
-    # Here we will study different calibration classes for different states of the models.
-    # First we create one global calibration class over the total time and name it 'global'.
-    # We can then use the simulations of this calibration class also for other ones which
-    # look at specific time intervals which are in the total interval of the global class.
-    # For the states of the models with specific time intervals we will use the
-    # calibration classes from the second example and customize them for the verbose
-    # sensitivity analysis, but we could also create custom classes directly here.
-    # As we don't need the second return value of the function (`validation_class`),
-    # we don't assign it do a variable (`, _`).
+    # To conduct a sensitivity analysis, we need to define calibration classes that
+    # encompass the objectives for which sensitivity is to be assessed. In this
+    # context, we'll explore distinct calibration classes corresponding to various
+    # states of the models. Initially, we establish a comprehensive calibration class
+    # that spans the entire duration, denoted as `global`. Subsequently, we can
+    # leverage simulations from this global class for other classes targeting
+    # specific time intervals within the overall global range.
+
+    # For the specific states of the models with distinct time intervals, we adopt the
+    # calibration classes from the second example and tailor them for the verbose
+    # sensitivity analysis. Alternatively, custom classes could be created directly
+    # at this stage. Given that we do not require the second return value of the
+    # function (`validation_class`), we opt to ignore it by using the variable
+    # assignment `_` and omit any associated text output of the second example.
+
     calibration_classes, _ = setup_calibration_classes(
         examples_dir=examples_dir, example=example, multiple_classes=False
     )
@@ -68,17 +73,18 @@ def run_sensitivity_analysis(
         examples_dir=examples_dir, example=example, multiple_classes=True
     )
     merged_calibration_classes.extend(merge_calibration_classes(calibration_classes))
-    # Now we have the following calibration classes and merged them directly.
+    # This results in the following calibration classes where we merge the time intervals directly.
     # We could have also merged them with an option in the `run()` function of the
     # sensitivity analyzer classes.
     print("Calibration classes for sensitivity analysis:",
           [c.name for c in merged_calibration_classes])
 
-    # For our verbose sensitivity analysis, we want all classes to have
-    # the same tuner-parameters. This comes so that we can use the same simulations
-    # for the sensitivity analysis of different calibration classes.
-    # But the last class of example B has different tuner parameters, so we reset
-    # them to the tuner parameters of the other classes.
+    # In our detailed sensitivity analysis, it is essential for all classes to share
+    # identical tuner parameters. This ensures that we can employ the same set of
+    # simulations for assessing sensitivity across various calibration classes.
+    # However, the final class in example B deviates in tuner parameters; hence, we
+    # reset them to align with the tuner parameters of the other classes.
+
     if example == 'B':
         merged_calibration_classes[-1].tuner_paras = merged_calibration_classes[0].tuner_paras
 
@@ -161,7 +167,7 @@ def run_sensitivity_analysis(
     # methods only return one dataframe, which is similar to the first return value
     # of the sobol method with possibly other analysis variables.
     # Let´s take a look at these results.
-    print("Result of the sensitivity analysis")
+
     # The first result has as columns the tuner-parameters
     # and a multi level index with three levels.
     # The first level defines the calibration class.
@@ -172,6 +178,7 @@ def run_sensitivity_analysis(
     # These analysis variables are specific for each method.
     # For their exact meaning I refer to the documentation of the SALib or the literature.
     # In this example you get a short overview in the comparison later.
+    print("Result of the sensitivity analysis")
     print('First and total order results of sobol method')
     print(result[0].to_string())  # TODO: Check if the to_string() method is better than simple result[0] in jupyter-notebook. If not, we could parse all functions of print(XX.to_string()) to XX in the example converter script.
     # The second result of the sobol method is for second order sensitive measures.
@@ -239,8 +246,7 @@ def run_sensitivity_analysis(
     # these plots, and you can also chang everything
     # on the axes of the plots.
     fig = plt.figure(figsize=plt.figaspect(1. / 4.), layout="constrained")  # creating one figure
-    subfigs = fig.subfigures(1, 3, wspace=0)  # creating subfigures for each type of plot
-    # plotting `S1` and `ST`
+    subfigs = fig.subfigures(1, 3, wspace=0)  # creating subfigures for each type of plot plotting `S1` and `ST`
     ax0 = subfigs[0].subplots()
     plotting.plot_single(
         result=result_sobol,
@@ -250,8 +256,7 @@ def run_sensitivity_analysis(
         figs_axes=([subfigs[0]], [ax0]),
         use_suffix=True,
     )
-    # plotting heatmap
-    ax1 = subfigs[1].subplots()
+    ax1 = subfigs[1].subplots()  # plotting heatmap
     plotting.heatmap(
         result_sobol_2,
         cal_class='global',
@@ -260,8 +265,7 @@ def run_sensitivity_analysis(
         show_plot=False,
         use_suffix=True
     )
-    # plotting the interactions of one single parameter
-    ax2 = subfigs[2].subplots()
+    ax2 = subfigs[2].subplots()  # plotting the interactions of one single parameter
     plotting.plot_single_second_order(
         result=result_sobol_2,
         para_name='rad.n',
@@ -292,6 +296,7 @@ def run_sensitivity_analysis(
     # These are just some basics, to understand what option you
     # have in `AixCaliBuAH`. For more information look up relevant literature.
 
+    # ## Comparison of different Methods
     # We will now take a short look at a comparison of the
     # `Sobol`, `Fast` and `Morris` method in `AixCaliBuAH`.
     # The `SALib` provides more methods, which can
